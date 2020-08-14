886e3114bba58a358066af4ddde6383b51f4e643
Guilherme Borges <guilhermerosasborges@gmail.com>
2020-08-14 01:38:06 +0100

    add c++ compatibility

--- dht.h
+++ dht.h
@@ -1,6 +1,14 @@
 #ifndef __DHT_H__
 #define __DHT_H__
 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
 int read_dht_data(int dht_pin, float* temperature, float* humidity);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
